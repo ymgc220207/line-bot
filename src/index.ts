165c9850441e69799ddf69156208eb6a29e2cde3
Kotaro Yamaguchi <66052219+ymgc220207@users.noreply.github.com>
2023-10-25 13:37:20 +0900

    :zap:メッセージのオウム返し機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
 import express from "express";
+import { middleware, messagingApi, WebhookEvent } from "@line/bot-sdk";
+import cors from "cors";
+
+const { MessagingApiClient } = messagingApi;
+
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
+  channelSecret: process.env.LINE_CHANNEL_SECRET || "",
+};
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use("/webhook", middleware(config));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/webhook", async (req, res) => {
+  const event: WebhookEvent = req.body.events[0];
+  if (event === undefined || event.type !== "message") {
+    res.sendStatus(200);
+    return;
+  }
+
+  let message = "";
+  if (event.message.type === "text") {
+    message = event.message.text;
+  }
+
+  const client = new MessagingApiClient({
+    channelAccessToken: config.channelAccessToken,
+  });
+
+  await client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [{ type: "text", text: message }],
+  });
+  res.sendStatus(200);
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}/`);
+  console.log(`Server is running on port ${PORT}`);
 });
